e15e46be9da1b64133148c4a112166d63cdf05b6
Tea Skela <dskela@jd20.law.harvard.edu>
2021-07-11 23:35:44 -0700

    Set up Twitter data route

---
+++ server/routes/twitter.js
@@ -0,0 +1,47 @@
+require('dotenv').config({ path: '../.env' });
+const router = require('express').Router();
+const axios = require('axios');
+
+router.get('/hashtag-data', async (req, res) => {
+  const options = {
+    method: 'GET',
+    url: `https://api.twitter.com/2/users/20702956/tweets?tweet.fields=created_at,entities,public_metrics&max_results=50`,
+    headers: {
+      'Authorization': `Bearer ${process.env.BEARER_TOKEN}`
+    }
+  }
+
+  try {
+    const results = await axios(options);
+
+    res.status(200).end(JSON.stringify(analyzeHashtags(results.data.data)))
+  } catch {
+
+    (err) => res.status(404).end('There was an error fetching Twitter data:', err)
+  }
+})
+
+var analyzeHashtags = function (data) {
+  var analytics = {};
+
+  for (var i = 0; i < data.length; i++) {
+    var hashtags = data[i].entities ? data[i].entities.hashtags || [] : [];
+    for (var j = 0; j < hashtags.length; j++) {
+      var hashtag = hashtags[j].tag;
+      if (analytics[hashtag]) {
+        analytics[hashtag].retweets += data[i].public_metrics.retweet_count;
+        analytics[hashtag].replies += data[i].public_metrics.reply_count;
+        analytics[hashtag].likes += data[i].public_metrics.like_count;
+      } else {
+        analytics[hashtag] = {};
+        analytics[hashtag].retweets = data[i].public_metrics.retweet_count;
+        analytics[hashtag].replies = data[i].public_metrics.reply_count;
+        analytics[hashtag].likes = data[i].public_metrics.like_count;
+      }
+    }
+  }
+
+  return analytics;
+}
+
+module.exports = router;
