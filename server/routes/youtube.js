ab02547bc50d6c2b9f8f7ca757718e27093ab8c8
Will Weindel <will.weindel@gmail.com>
2021-07-14 09:36:40 -0700

    merges latest pull

--- server/routes/youtube.js
+++ server/routes/youtube.js
@@ -15,14 +15,14 @@ router.get('/channel-stats', async (req, res) => {
 });
 
 //gets video Ids by channel Id
-router.get('/video', (req, res) => {
+router.post('/video', (req, res) => {
   try {
     console.log(req.body, req.query, req.params);
 
-    const videos = axios.get(`https://www.googleapis.com/youtube/v3/search?key=${process.env.GOOGLE_API}&channelId=${req.query.channelId}&part=id&order=date`).then(data => {
+    const videos = axios.get(`https://www.googleapis.com/youtube/v3/search?key=${process.env.GOOGLE_API}&channelId=${req.body.channelId}&part=id&order=date`).then(data => {
 
       const mappedVideos = data.data.items.map( video => {
-        console.log(`https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&id=${video.id.videoId}&key=${process.env.GOOGLE_API}`)
+        console.log(`https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&id=${video.id.videoId}&key=${process.env.GOOGLE_API}`);
         return axios.get(`https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&id=${video.id.videoId}&key=${process.env.GOOGLE_API}`);
       });
       Promise.all(mappedVideos)
