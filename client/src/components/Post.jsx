9e115d56a261243bf11a9ef31f1a4e7ef8422276
godfreydoo <godfrey.doo@gmail.com>
2021-07-14 10:40:25 -0700

    Add post functionality to Post component

--- client/src/components/Post.jsx
+++ client/src/components/Post.jsx
@@ -1,24 +1,53 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Button from '@material-ui/core/Button';
 import TextField from '@material-ui/core/TextField';
 import TextareaAutosize from '@material-ui/core/TextareaAutosize';
 import SendIcon from '@material-ui/icons/Send';
 import { makeStyles } from '@material-ui/core/styles';
-
-
+import Cookies from 'js-cookie';
+import axios from 'axios';
 
 const Post = function() {
+
+  const [tweet, setTweet] = useState('');
+
+  const handleOnChange = (e) => {
+    setTweet(e.target.value);
+  };
+
+  const postTweet = function () {
+    var token = Cookies.get('twitter-auth-request');
+    let config = {
+      method: 'post',
+      url: '/twitter/tweet',
+      header: {
+        Authorization: `Bearer ${token}`
+      },
+      data: tweet
+    };
+    console.log(config);
+    axios(config)
+      .then(() => {
+        console.log('Tweet has posted');
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  };
+
   return (
     <>
       <div>
         <TextField
           id="outlined-helperText"
           label="Message"
-          defaultValue="Type your message here!"
+          placeholder="Type your message here!"
           variant="outlined"
           multiline={true}
           rows={4}
           style={{width: 300}}
+          value={tweet}
+          onChange={handleOnChange}
         />
       </div>
       <div>
@@ -26,6 +55,7 @@ const Post = function() {
           endIcon={<SendIcon />}
           variant="contained"
           color="primary"
+          onClick={postTweet}
         >
           Send
         </Button>
