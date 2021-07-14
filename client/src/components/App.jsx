567267f20812069d4e99a6a2a032a4052753838a
Will Weindel <will.weindel@gmail.com>
2021-07-14 09:31:54 -0700

    gets twitter data

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -22,7 +22,6 @@ const App = props => {
 
   //currently uses hardcoded user info - will need to update to session/cookie info
   const getTwitterData = function() {
-    console.log(reqq);
     let config = {
       method: 'get',
       url: '/twitter/home-timeline',
