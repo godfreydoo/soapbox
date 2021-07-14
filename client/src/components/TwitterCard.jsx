94253f5dc524fabef01b4ce7be39caf13844c5da
Francis Yao <neko.makie27@gmail.com>
2021-07-14 09:17:01 -0700

    prop validation

--- client/src/components/TwitterCard.jsx
+++ client/src/components/TwitterCard.jsx
@@ -7,9 +7,11 @@ import CardContent from '@material-ui/core/CardContent';
 import CardActions from '@material-ui/core/CardActions';
 import Avatar from '@material-ui/core/Avatar';
 import TwitterIcon from '@material-ui/icons/Twitter';
+import PropTypes from 'prop-types';
 
 const cardStyles = makeStyles((theme) => ({
   root: {
+    borderRadius: 15,
     width: 360,
   },
   media: {
@@ -22,34 +24,38 @@ const YTurl = 'https://www.youtube.com/embed/tVCYa_bnITg';
 
 const TwitterCard = function(props) {
   const classes = cardStyles();
-  const { text: message, user: {profile_image_url: avatar, name, location, screen_name}} = props.tweet;
+  const { text: message, user: {profile_image_url: avatar, name, location, screen_name: screenname}} = props.tweet;
   console.log(props.tweet);
   return (
     <>
-    {avatar && (<Card className={classes.root}>
-      <CardHeader
-        title={name}
-        subheader={`@${screen_name}`}
-        avatar={
-          <Avatar src={avatar}/>
-        }
-        action={
-          <TwitterIcon
-            fontSize="large"
-            style={{color: '00ACEE'}}
-          />
-        }
-      />
-      {/* <CardMedia
+      {avatar && (<Card className={classes.root}>
+        <CardHeader
+          title={name}
+          subheader={`@${screenname}`}
+          avatar={
+            <Avatar src={avatar}/>
+          }
+          action={
+            <TwitterIcon
+              fontSize="large"
+              style={{color: '00ACEE'}}
+            />
+          }
+        />
+        {/* <CardMedia
         className={classes.media}
         image="https://i.imgur.com/kc9gSnm.png"
       /> */}
-      <CardContent>
-        {message}
-      </CardContent>
-    </Card>)}
+        <CardContent>
+          {message}
+        </CardContent>
+      </Card>)}
     </>
   );
+};
+
+TwitterCard.propTypes = {
+  tweet: PropTypes.object,
 };
 
 export default TwitterCard;
