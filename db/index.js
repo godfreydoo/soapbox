aa9ec296d5af92aaa54a7973fece5fe296fe919d
godfreydoo <godfrey.doo@gmail.com>
2021-07-17 03:28:43 -0700

    WORK

--- db/index.js
+++ db/index.js
@@ -12,11 +12,12 @@ mongoDB.once('open', () => {
   console.log('mongoose connected successfully');
 });
 
+const redis = require('redis');
+const redisPort = 6379;
+let redisClient;
 
 if (process.env.NODE_ENV !== 'test') {
-  const redis = require('redis');
-  const redisPort = 6379;
-  const redisClient = redis.createClient(redisPort);
+  redisClient = redis.createClient(redisPort);
   redisClient.on('error', function(error) {
     if (error) {
       console.log('Redis client is not connected ', error);
